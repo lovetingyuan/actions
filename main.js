0e35617fc2f58aa3dcddd78af22aa2f82393594f
tingyuan <hbtwhucsis@gmail.com>
2022-07-12 23:26:10 +0800

    chore

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 const { get, post } = require('./request');
 
 function main() {
-  return get('https://api.bilibili.com/x/space/acc/info?mid=1458143131&jsonp=jsonp').then(data => {
-    if (data.code) {
-      return pushMessage(data.message);
+  return get(`https://api.bilibili.com/x/space/acc/info?mid=${process.env.UP_ID}&jsonp=jsonp`).then(
+    data => {
+      if (data.code) {
+        return pushMessage(data.message);
+      }
+      const { liveStatus, url, title } = data.data.live_room;
+      if (liveStatus) {
+        return pushMessage(title, url);
+      }
     }
-    const { liveStatus, url, title } = data.data.live_room;
-    if (liveStatus) {
-      return pushMessage(title, url);
-    }
-  });
+  );
 }
 
 function pushMessage(title, url = '') {
+  const summary = Buffer.from(
+    '57+g57+g55u05pKt5ZWm77yBbygq77+j4pa977+jKinjg5Y=',
+    'base64'
+  ).toString('utf8');
   return post('https://wxpusher.zjiecode.com/api/send/message', {
     appToken: process.env.WX_PUSHER_TOKEN,
     content: title,
-    summary: '翠翠直播啦！o(*￣▽￣*)ブ', //消息摘要，显示在微信聊天页面或者模版消息卡片上，限制长度100，可以不传，不传默认截取content前面的内容。
+    summary, //消息摘要，显示在微信聊天页面或者模版消息卡片上，限制长度100，可以不传，不传默认截取content前面的内容。
     contentType: 1, //内容类型 1表示文字  2表示html(只发送body标签内部的数据即可，不包括body标签) 3表示markdown
     // topicIds: [
     //   //发送目标的topicId，是一个数组！！！，也就是群发，使用uids单发的时候， 可以不传。
@@ -33,22 +39,7 @@ function pushMessage(title, url = '') {
   });
 }
 
-if (process.argv.includes('--test')) {
-  pushMessage('test hehehe').catch(err => {
-    console.error(err);
-    process.exit(-1);
-  });
-} else {
-  main().catch(err => {
-    console.error(err);
-    process.exit(-1);
-  });
-}
-const a = process.env['WX_PUSHER_TOKEN'];
-console.log(333, a);
-console.log(
-  22222,
-  process.env,
-  typeof process.env.WX_PUSHER_TOKEN,
-  process.env.WX_PUSHER_TOKEN.length
-);
+main().catch(err => {
+  console.error(err);
+  process.exit(-1);
+});
